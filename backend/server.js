01f0e8d0eb586c399f869974bd275f07010b6874
Elizabeth Clawson <emclawson1@gmail.com>
2024-10-03 12:15:05 -0500

    setup backend

---
+++ backend/server.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import cors from 'cors';
+
+const app = express()
+
+app.use(cors())
+
+const PORT = 8080
+
+app.get('/test', (req, res) => {
+    res.json("hello")
+})
+
+app.listen(PORT, () => {
+    console.log(`listening on port ${PORT}`)
+})
