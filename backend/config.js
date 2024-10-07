1ec51c0a887f658f763c9b17efee6a2fdd2420e7
Elizabeth Clawson <emclawson1@gmail.com>
2024-10-07 10:26:37 -0500

    added connectdb function to config.js - doesn't include connection link yet

--- backend/config.js
+++ backend/config.js
@@ -1,1 +1,12 @@
 import mongoose from "mongoose";
+
+async function connectDB() {
+    try{
+        mongoose.connect()
+        console.log("mongoDV connected")
+    } catch(e) {
+        console.log(e)
+    }
+}
+
+export default connectDB
