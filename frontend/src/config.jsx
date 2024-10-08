d3f7a90115ab878cea5a4ff362c59367b68bacb6
Elizabeth Clawson <emclawson1@gmail.com>
2024-10-08 12:22:17 -0500

    added firbase configuration

--- frontend/src/config.jsx
+++ frontend/src/config.jsx
@@ -3,15 +3,8 @@ import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyDS2bacq1mLqg-1lu9nYZBj6EWbPzeg2Lg",
-  authDomain: "bridgehouse-project.firebaseapp.com",
-  projectId: "bridgehouse-project",
-  storageBucket: "bridgehouse-project.appspot.com",
-  messagingSenderId: "213285827633",
-  appId: "1:213285827633:web:46ecc287e28a0dc7d89110"
-};
+//importing our hidden config
+import config from "./firebaseconfig"
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+const app = initializeApp(config);
