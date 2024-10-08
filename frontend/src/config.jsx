ae7a492972c4a9da4df52474439a7b07ed7b0596
Elizabeth Clawson <emclawson1@gmail.com>
2024-10-08 12:01:41 -0500

    added config file with firebase boilerplate config code

---
+++ frontend/src/config.jsx
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDS2bacq1mLqg-1lu9nYZBj6EWbPzeg2Lg",
+  authDomain: "bridgehouse-project.firebaseapp.com",
+  projectId: "bridgehouse-project",
+  storageBucket: "bridgehouse-project.appspot.com",
+  messagingSenderId: "213285827633",
+  appId: "1:213285827633:web:46ecc287e28a0dc7d89110"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
