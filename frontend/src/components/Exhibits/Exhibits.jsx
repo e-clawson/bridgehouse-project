6feff5f5942a991ac93080f3e0224decf40d1dd0
Elizabeth Clawson <emclawson1@gmail.com>
2024-10-14 11:41:00 -0400

    added to exhibit card

--- frontend/src/components/Exhibits/Exhibits.jsx
+++ frontend/src/components/Exhibits/Exhibits.jsx
@@ -14,6 +14,7 @@ export default function Exhibits({currentUser}) {
   const [isEditing, setIsEditing] = useState(false)
   const [editId, setEditId] = useState(null);
   const [editText, setEditText] = useState("");
+  const [selectedTags, setSelectedTags] = useState("")
   const [formData, setFormData] = useState({
     title: "", 
     subtitle: "", 
@@ -39,11 +40,21 @@ export default function Exhibits({currentUser}) {
     test()
   }, [])
 
+  function checkboxHandler(e){
+    let isSelected = e.target.checked
+    let value = e.target.value
+    if (isSelected){
+      setSelectedTags(...selectedTags, value)
+    }
+  }
   //allows you to change the input text 
   function handleChange(e){ 
+   
     const { name, value } = e.target;
     setFormData((formData) => ({...formData, [name]: value}))
   }
+
+  
 
   //handles the button click of the form 
   async function handleSubmit(e){
@@ -53,14 +64,14 @@ export default function Exhibits({currentUser}) {
     const exhibit = {
       title: formData.title, 
       subtitle: formData.subtitle, 
-      floor: input, 
-      dateNum: input, 
-      dateString: input, 
-      image: input,
-      imgCaption: input,
-      pageContent: input, 
-      additionalResources: input, 
-      tags: input, 
+      floor: formData.floor, 
+      dateNum: formData.dateNum, 
+      dateString: formData.dateString, 
+      image: formData.image,
+      imgCaption: formData.imgCaption,
+      pageContent: formData.pageContent, 
+      additionalResources: formData.additionalResources, 
+      tags: formData.tags, 
     }
     //make the POST request - create a new exhibit display
     const response = await fetch(`${BASE_URL}/exhibits`, {
@@ -76,6 +87,7 @@ export default function Exhibits({currentUser}) {
     console.log(exhibits)
     //once everything is done, reset the form to empty
     setInput("")
+    //notreaching this setInput for some reason 
   }
 
   //make the DELETE request - delete an exhibit by ID 
@@ -143,14 +155,25 @@ export default function Exhibits({currentUser}) {
              ) : (
                <>
                  <p>{exhibit.title}</p> 
+                 <h2>{exhibit.title}</h2> 
+                 <h4>{exhibit.subtitle}</h4> 
+                 <img src={exhibit.image}></img>
+                 <p>{exhibit.imgCaption}</p>
+                 <p>{exhibit.pageContent}</p>
+                 <p>{exhibit.additionalResources}</p>
                  <button onClick={() => handleDelete(exhibit._id)}>Delete</button>
                  <button onClick={() => handleEditClick(exhibit._id, exhibit.text)}>Edit</button>
                </>
              )}
             </div> : 
-            <>
-              <p>{exhibit.title}</p> 
-            </>}
+            <div key={exhibit._id} className='exhibit-card'>
+              <h2>{exhibit.title}</h2> 
+              <h4>{exhibit.subtitle}</h4> 
+              <img src={exhibit.image}></img>
+              <p>{exhibit.imgCaption}</p>
+              <p>{exhibit.pageContent}</p>
+              <p>{exhibit.additionalResources}</p>
+            </div>}
             
           </div>
         )}
@@ -159,49 +182,76 @@ export default function Exhibits({currentUser}) {
       <div>
       { currentUser !== null ? 
       <div className="exhibit-form">
-        <form onSubmit={handleSubmit}>
+        <form id="exhibitForm" onSubmit={handleSubmit}>
           <div>
             <h3>Add A Topic:</h3>
               <div>
-                <label >Title</label>
+                <label>Title
                 <input value={formData.title} name="title" id="title" onChange={handleChange}/>
+                </label>
               </div>
               <div>
-                <label >Sub-Title</label>
+                <label >Sub-Title
                 <input value={formData.subtitle} name="subtitle" id="subtitle" onChange={handleChange}/>
+                </label>
               </div>
               <div>
-                <label >Museum Floor</label>
-                <input value={formData.floor} name="floor" id="floor" onChange={handleChange}/>
+                <label >Museum Floor
+                <select value={formData.floor} name="floor" id="floor" onChange={handleChange}>
+                  <option disabled value="">Select...</option>
+                  <option value="0" >0 </option>
+                  <option value="1">1 </option>
+                  <option value="2">2 </option>
+                  <option value="3">3 </option>
+                  <option value="4">4 </option>
+                  <option value="5">5 </option>
+                </select>
+                </label>
               </div>
               <div>
-                <label >Date (number only)</label>
-                <input value={formData.dateNum} name="dateNum" id="dateNum" onChange={handleChange}/>
+                <label>Date (number only: mm-dd-yyyy)
+                <input type="date" value={formData.dateNum} name="dateNum" id="dateNum" onChange={handleChange}/>
+                </label>
               </div>
               <div>
-                <label > date (text and numbers)</label>
+                <label> Date in text and numbers (ex: "March 1, 1900")
                 <input value={formData.dateString} name="dateString" id="dateString" onChange={handleChange}/>
+                </label>
               </div>
               <div>
-                <label > Image</label>
+                <label> Image
                 <input type="file" value={formData.image} name="image" id="image" onChange={handleChange}/>
+                </label>
               </div>
               <div>
-                <label >Image Caption</label>
+                <label>Image Caption
                 <input value={formData.imgCaption} name="imgCaption" id="imgCaption" onChange={handleChange}/>
+                </label>
               </div>
               <div>
-                <label >Page Content</label>
-                <input value={formData.pageContent} name="pageContent" id="pageContent" onChange={handleChange}/>
+                <label>Page Content
+                <textarea value={formData.pageContent} name="pageContent" id="pageContent" onChange={handleChange}/>
+                </label>
               </div>
               <div>
-                <label >Additional Resources (links, citations)</label>
+                <label>Additional Resources (links, citations)
                 <input value={formData.additionalResources} name="additionalResources" id="additionalResources" onChange={handleChange}/>
+                </label>
               </div>
               <div>
-                <label >Tags (Please use: Science, Nature, History, Public Works/Health, Bridges)</label>
+                <label>Tags (Please use only the following: Architecture, Bridges and Engineering, History, Nature, Public Health, Other )
                 <input value={formData.tags} name="tags" id="tags" onChange={handleChange}/>
+                </label>
               </div>
+              {/* <div>
+              <p>Tags</p>
+                  <input type="checkbox" value="Architecture" onChange={checkboxHandler}>Architecture</input>
+                  <input type="checkbox" value="Bridges and Engineering" onChange={checkboxHandler}>Bridges and Engineering</input>
+                  <input type="checkbox" value="History" onChange={checkboxHandler}>History</input> 
+                  <input type="checkbox" value="Nature" onChange={checkboxHandler}>Nature</input> 
+                  <input type="checkbox" value="Public Health" onChange={checkboxHandler}>Public Health</input>
+                  <input type="checkbox" value="Other" onChange={checkboxHandler}>Other</input> 
+              </div> */}
             </div>
           <button>Add</button>
         </form>
