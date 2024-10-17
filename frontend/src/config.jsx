9f3e6937df6bbbb5fcce12472f77693f0f8632d8
Elizabeth Clawson <emclawson1@gmail.com>
2024-10-16 20:09:20 -0500

    updated firebaseconfig

--- frontend/src/config.jsx
+++ frontend/src/config.jsx
@@ -3,7 +3,14 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth"
 
 //importing our hidden config
-import config from "./firebaseconfig"
+const firebaseConfig = {
+    apiKey: "AIzaSyDS2bacq1mLqg-1lu9nYZBj6EWbPzeg2Lg",
+    authDomain: "bridgehouse-project.firebaseapp.com",
+    projectId: "bridgehouse-project",
+    storageBucket: "bridgehouse-project.appspot.com",
+    messagingSenderId: "213285827633",
+    appId: "1:213285827633:web:46ecc287e28a0dc7d89110"
+  };
 
 // Initialize Firebase
 const app = initializeApp(config);
