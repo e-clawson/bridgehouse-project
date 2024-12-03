f5d6f028b3933c3b65869358f92653900e940ef2
Elizabeth Clawson <emclawson1@gmail.com>
2024-12-03 12:17:08 -0600

    added content to footer

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -18,7 +18,26 @@ export default function Footer({currentUser, setCurrentUser}){
         <>
         <div className="footer">
             <h4>Sitemap:</h4>
-            { currentUser === null ? <Link to="/login">Admin Login</Link> : <Link onClick={logout} to="/">Sign Out</Link> }
+            <Link to="/about">About</Link>
+            <Link>Careers</Link>
+            <Link to="/contact">Contact</Link>
+            <Link to="/events">Events</Link>
+            <Link to="/exhibits">Exhibits</Link>
+            <Link to="/home">Home</Link>
+            <Link to="/support">Support</Link>
+            <Link to="/venue-rentals">Venue Rentals</Link>
+
+            <br></br>
+            <h6>The Chicago Bridgehouse Museum is part of the Friends of the Chicago River</h6>
+            <Link to="https://www.chicagoriver.org/">Friends Website</Link>
+            <br></br>
+
+            <h6><b>General Info:</b></h6>
+            <p><b>Location:</b> South West Bridgetower at Michigan Ave. & Wacker Dr. - 99 Chicago Riverwalk, Chicago, IL 60601
+            <br/><b>Hours:</b> Open Seasonally, May - October, Wednesday - Sunday, 10am - 4pm
+            <br/><b>Phone:</b>(312) 977-0227</p>
+
+            { currentUser === null ? <Link to="/login">Admin: Login</Link> : <Link onClick={logout} to="/">Admin: Sign Out</Link> }
         </div>
         </>
     )
