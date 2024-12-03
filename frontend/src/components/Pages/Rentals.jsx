757e9ad07fc3b6df5513c3f6becbbe17b88de649
Elizabeth Clawson <emclawson1@gmail.com>
2024-12-03 10:31:08 -0600

    tried to add photos to the bootstrap slide

--- frontend/src/components/Pages/Rentals.jsx
+++ frontend/src/components/Pages/Rentals.jsx
@@ -2,6 +2,7 @@ import Navbar from "../Navbar/Navbar"
 import Header from "../Header/Header"
 import Footer from "../Footer/Footer"
 
+
 export default function Rentals(){
     return (
     <>
@@ -13,30 +14,43 @@ export default function Rentals(){
                  <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
                  <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
                  <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                 <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="3" aria-label="Slide 4"></button>
              </div>
              <div class="carousel-inner">
              <div class="carousel-item active">
-                 <img src="..." class="d-block w-100" alt="..."/>
+                 <img src= "../../assets/rentals/Wedding.png" class="d-block w-100" alt="Wedding Taking Place at the Bridgehouse Museum"/>
              <div class="carousel-caption d-none d-md-block">
-             <h5>First slide label</h5>
-             <p>Some representative placeholder content for the first slide.</p>
+             <h5>Weddings</h5>
+             <p>Wow your wedding guests with a purely Chicago experience!</p>
              </div>
              </div>
+
              <div class="carousel-item">
-             <img src="..." class="d-block w-100" alt="..."/>
+             <img src="/Users/elizabethclawson/Desktop/per-scholas-work/bridgehouse-project/frontend/src/assets/rentals/table.png" class="d-block w-100" alt="Image of a fancy Dinner table setup in the top of the Bridgehouse Tower "/>
              <div class="carousel-caption d-none d-md-block">
-                 <h5>Second slide label</h5>
-                 <p>Some representative placeholder content for the second slide.</p>
+                 <h5>Private Events and Dinners</h5>
+                 <p>The perfect place for a romantic dinner for two or a festive gathering of fifty</p>
              </div>
              </div>
+
              <div class="carousel-item">
-             <img src="..." class="d-block w-100" alt="..."/>
+             <img src="/Users/elizabethclawson/Desktop/per-scholas-work/bridgehouse-project/frontend/src/assets/rentals/Outside.png" class="d-block w-100" alt="Image looking down from above on a gathering out on the patio"/>
              <div class="carousel-caption d-none d-md-block">
-                 <h5>Third slide label</h5>
-                 <p>Some representative placeholder content for the third slide.</p>
+                 <h5>Parties</h5>
+                 <p>A party in our historic landmark bridgetower is one that your guests will never forget!</p>
              </div>
              </div>
+
+             <div class="carousel-item">
+             <img src="../frontend/src/assets/rentals/first floor.png" class="d-block w-100" alt="People gathering and eating and mingling on the first floor of the museum"/>
+             <div class="carousel-caption d-none d-md-block">
+                 <h5>Corporate Events and Meetings</h5>
+                 <p>Host your next work event, meeting, or mixer in an iconic Chicago Landmark</p>
              </div>
+             </div>
+             
+             </div>
+             
              <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
                  <span class="carousel-control-prev-icon" aria-hidden="true"></span>
                  <span class="visually-hidden">Previous</span>
@@ -45,7 +59,7 @@ export default function Rentals(){
                  <span class="carousel-control-next-icon" aria-hidden="true"></span>
                  <span class="visually-hidden">Next</span>
              </button>
-         </div>
+        </div>
     
     </>
     )
