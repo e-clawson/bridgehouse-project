dd0a716868ac4207eafde6299ce7679ed0192ee2
Elizabeth Clawson <emclawson1@gmail.com>
2024-12-03 10:50:31 -0600

    added images and information to rental page

--- frontend/src/components/Pages/Rentals.jsx
+++ frontend/src/components/Pages/Rentals.jsx
@@ -8,6 +8,7 @@ export default function Rentals(){
     <>
         <div className="rentals">
             <h1>Rentals</h1>
+            <br></br>
         </div>
         <div id="carouselExampleCaptions" class="carousel slide">
              <div class="carousel-indicators">
@@ -16,41 +17,43 @@ export default function Rentals(){
                  <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
                  <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="3" aria-label="Slide 4"></button>
              </div>
+
              <div class="carousel-inner">
+
              <div class="carousel-item active">
-                 <img src= "../../assets/rentals/Wedding.png" class="d-block w-100" alt="Wedding Taking Place at the Bridgehouse Museum"/>
-             <div class="carousel-caption d-none d-md-block">
-             <h5>Weddings</h5>
-             <p>Wow your wedding guests with a purely Chicago experience!</p>
-             </div>
-             </div>
-
-             <div class="carousel-item">
-             <img src="/Users/elizabethclawson/Desktop/per-scholas-work/bridgehouse-project/frontend/src/assets/rentals/table.png" class="d-block w-100" alt="Image of a fancy Dinner table setup in the top of the Bridgehouse Tower "/>
-             <div class="carousel-caption d-none d-md-block">
-                 <h5>Private Events and Dinners</h5>
-                 <p>The perfect place for a romantic dinner for two or a festive gathering of fifty</p>
-             </div>
+                 <img src="/src/assets/Wedding.png" class="d-block w-100" alt="Wedding Taking Place at the Bridgehouse Museum"/>
+                <div class="carousel-caption d-none d-md-block">
+                     <h5>Weddings</h5>
+                    <p>Wow your wedding guests with a purely Chicago experience!</p>
+                 </div>
              </div>
 
              <div class="carousel-item">
-             <img src="/Users/elizabethclawson/Desktop/per-scholas-work/bridgehouse-project/frontend/src/assets/rentals/Outside.png" class="d-block w-100" alt="Image looking down from above on a gathering out on the patio"/>
-             <div class="carousel-caption d-none d-md-block">
-                 <h5>Parties</h5>
-                 <p>A party in our historic landmark bridgetower is one that your guests will never forget!</p>
-             </div>
+                <img src="/src/assets/rentals/table.png" class="d-block w-100" alt="Image of a fancy Dinner table setup in the top of the Bridgehouse Tower "/>
+                 <div class="carousel-caption d-none d-md-block">
+                    <h5>Private Events and Dinners</h5>
+                    <p>The perfect place for a romantic dinner for two or a festive gathering of fifty</p>
+                </div>
              </div>
 
              <div class="carousel-item">
-             <img src="../frontend/src/assets/rentals/first floor.png" class="d-block w-100" alt="People gathering and eating and mingling on the first floor of the museum"/>
-             <div class="carousel-caption d-none d-md-block">
-                 <h5>Corporate Events and Meetings</h5>
-                 <p>Host your next work event, meeting, or mixer in an iconic Chicago Landmark</p>
+                <img src="/src/assets/rentals/Outside.png" class="d-block w-100" alt="Image looking down from above on a gathering out on the patio"/>
+                <div class="carousel-caption d-none d-md-block">
+                    <h5>Parties</h5>
+                    <p>A party in our historic landmark bridgetower is one that your guests will never forget!</p>
+                 </div>
              </div>
+
+             <div class="carousel-item">
+                <img src="src/assets/rentals/firstFloor.png" class="d-block w-100" alt="People gathering and eating and mingling on the first floor of the museum"/>
+                <div class="carousel-caption d-none d-md-block">
+                    <h5>Corporate Events and Meetings</h5>
+                    <p>Host your next work event, meeting, or mixer in an iconic Chicago Landmark</p>
+                </div>
              </div>
-             
+
              </div>
-             
+
              <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
                  <span class="carousel-control-prev-icon" aria-hidden="true"></span>
                  <span class="visually-hidden">Previous</span>
@@ -60,6 +63,35 @@ export default function Rentals(){
                  <span class="visually-hidden">Next</span>
              </button>
         </div>
+        <br></br>
+
+        
+
+        <div className="rental-text"> 
+            <h2 class="border border-2 border-dark-subtle"> Information: </h2>
+            <h5>Capacity: 79</h5>
+            
+            <h5>Rates:</h5>
+            <p>Monday - Thursday: $350/hr
+            <br></br>
+            Friday - Sunday: $375/hr 
+            <br></br>
+            (8 A.M. -11 P.M.)</p>
+           
+            <h5>Preferred Caterers:</h5>
+            <ul>
+                <li>Inspired By Catering</li>
+                <li>Blue Plate</li>
+                <li>Purple Pig</li>
+            </ul>
+
+
+
+
+
+
+        </div>
+
     
     </>
     )
