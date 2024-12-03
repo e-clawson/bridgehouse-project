b0fc8762485a00c99f4fb553ad52920c44a932c8
Elizabeth Clawson <emclawson1@gmail.com>
2024-12-03 12:41:07 -0600

    added styling to about page

--- frontend/src/components/Pages/About.jsx
+++ frontend/src/components/Pages/About.jsx
@@ -4,20 +4,26 @@ import Footer from "../Footer/Footer"
 
 export default function About(){
     return (
-         <div className="bridge-lifts" class="padding">
-            
+         <div className="bridge-lifts padding ">
+            <div className="padding">
             <h1>About</h1>
+            <br></br>
             <img src="../src/assets/homepage/ThirdFloor.png" class="center" alt="Image of the Museum with the Bridge lifting in the background"/>
             <br></br>
             <p>The McCormick Bridgehouse & Chicago River Museum celebrates the Chicago River and its world-famous movable bridges.
             Beginning at river level and spiraling five stories up, the McCormick Bridgehouse & Chicago River Museum provides a one-of-a-kind opportunity to explore a historic landmark bridgehouse.</p>
             <p>At river level, visitors can view the massive gears of Chicago’s most famous movable bridge and then journey through time as they experience the story of the Chicago River. Once at the top of the Bridgehouse, visitors are treated to magnificent 360 degree views of the city and river, and will gain a whole new perspective on the beauty and complexity that is Chicago.</p>
             <p>The Bridgehouse Museum furthers the mission of Friends of the Chicago River.</p>
+            </div>
 
+            <div className="background">
             <h3>Plan Your Visit:</h3>
-            <h5>Hours of Operation: Open Seasonally, May - October, Wednesday - Sunday, 10am - 4pm</h5>
-            <h5>Location: South West Bridgetower at Michigan Ave. & Wacker Dr. - 99 Chicago Riverwalk, Chicago, IL 60601</h5>
-            <h5>Phone: (312) 977-0227</h5>
+            <br></br>
+            <h5><b>Hours of Operation:</b> Open Seasonally, May - October, Wednesday - Sunday, 10am - 4pm</h5>
+            <h5><b>Location:</b> South West Bridgetower at Michigan Ave. & Wacker Dr. - 99 Chicago Riverwalk, Chicago, IL 60601</h5>
+            <h5><b>Phone:</b> (312) 977-0227</h5>
+            </div>
+            
 
         </div>
     )
