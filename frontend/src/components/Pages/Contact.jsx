4c129a1676d0d3fd6d64142489c52f3db61d93e6
Elizabeth Clawson <emclawson1@gmail.com>
2024-12-03 12:19:33 -0600

    added sample contact form

--- frontend/src/components/Pages/Contact.jsx
+++ frontend/src/components/Pages/Contact.jsx
@@ -7,6 +7,20 @@ export default function Contact(){
          <div className="contact" class="padding">
            
             <h1>Contact Us:</h1>
+
+            <div class="mb-3">
+                <label for="exampleFormControlInput1" class="form-label">Name</label>
+                <input type="name" class="form-control" id="name" placeholder="Name"/>
+            </div>
+            <div class="mb-3">
+                <label for="exampleFormControlInput1" class="form-label">Email Address</label>
+                <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com"/>
+            </div>
+            <div class="mb-3">
+            <label for="exampleFormControlTextarea1" class="form-label">Message</label>
+            <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+            </div>
+            <button>Submit</button>
          
         </div>
     )
